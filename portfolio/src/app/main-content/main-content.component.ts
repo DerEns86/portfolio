f2a37cbba9cda7b3aa56e9b3963d38a0dcb35c04
Viktor Ens <v.ens86@yahoo.de>
2024-01-29 21:29:28 +0100

    components added

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjektsComponent } from './projekts/projekts.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, LandingPageComponent],
+  imports: [HeaderComponent, FooterComponent, LandingPageComponent, AboutMeComponent, SkillsComponent, ProjektsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
