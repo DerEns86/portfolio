1fdfc790b1bd3e434510f835e6557248cb781a34
Viktor Ens <v.ens86@yahoo.de>
2024-01-30 11:52:03 +0100

    skill-card.component added

--- portfolio/src/app/main-content/skills/skills.component.ts
+++ portfolio/src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { SkillCardComponent } from './skill-card/skill-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ,SkillCardComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    { name: 'Angular', iconPath: 'angular.png' },
+    { name: 'Typescript', iconPath: 'ts.png' },
+  ]
 }
