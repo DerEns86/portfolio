3a0ca818e70afe68f7794008bf137613006ff30f
Viktor Ens <v.ens86@yahoo.de>
2024-01-30 20:08:58 +0100

    added mySkills content

--- portfolio/src/app/main-content/skills/skills.component.ts
+++ portfolio/src/app/main-content/skills/skills.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { SkillCardComponent } from './skill-card/skill-card.component';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [ CommonModule ,SkillCardComponent],
+  imports: [ CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
@@ -13,5 +13,13 @@ export class SkillsComponent {
   skills = [
     { name: 'Angular', iconPath: 'angular.png' },
     { name: 'Typescript', iconPath: 'ts.png' },
+    { name: 'Javascript', iconPath: 'js.png' },
+    { name: 'HTML', iconPath: 'ts.png' },
+    { name: 'CSS', iconPath: 'css.png' },
+    { name: 'Git', iconPath: 'git.png' },
+    { name: 'Firebase', iconPath: 'firebase.png' },
+    { name: 'Scrum', iconPath: 'scrum.png' },
+    { name: 'Rest-API', iconPath: 'api.png' },
+    { name: 'Matiral Design', iconPath: 'material.png' },
   ]
 }
---
+++ portfolio/src/app/models/skills.ts
@@ -0,0 +1,17 @@
+export class Skills {
+    public name: String = '';
+    public iconPath: String = '';
+}
+
+// const skills = [
+//     { name: 'Angular', iconPath: 'angular.png' },
+//     { name: 'Typescript', iconPath: 'ts.png' },
+//     { name: 'Javascript', iconPath: 'js.png' },
+//     { name: 'HTML', iconPath: 'ts.png' },
+//     { name: 'CSS', iconPath: 'css.png' },
+//     { name: 'Git', iconPath: 'git.png' },
+//     { name: 'Firebase', iconPath: 'firebase.png' },
+//     { name: 'Scrum', iconPath: 'scrum.png' },
+//     { name: 'Rest-API', iconPath: 'api.png' },
+//     { name: 'Matiral Design', iconPath: 'material.png' },
+//   ]
