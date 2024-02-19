fdef60ae05a11419f3fba136361b624539c6dfac
Viktor Ens <v.ens86@yahoo.de>
2024-02-19 13:34:24 +0100

    sendMail function added

--- portfolio/src/app/main-content/contact/contact.component.ts
+++ portfolio/src/app/main-content/contact/contact.component.ts
@@ -1,26 +1,56 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
  
-  contactData = {
+constructor(){}
+ contactData = {
     name: '',
     email: '',
     message: '',
     checked: false,
   }
+  
+http = inject(HttpClient)
 
-  onSubmit(ngForm : NgForm){
-    if(ngForm.valid && ngForm.submitted){
-    console.log(this.contactData)
-    }
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://v-ens.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid ) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            console.log(response)
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } 
   }
+
+ 
+
 }
