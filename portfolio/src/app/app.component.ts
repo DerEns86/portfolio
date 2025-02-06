78d22553cc6e196b493d4ca91628c0c981be3b98
DerEns86 <v.ens86@yahoo.de>
2025-02-06 15:45:00 +0100

    Add header and footer components to app layout; update project data formatting

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -4,13 +4,21 @@ import { RouterOutlet } from '@angular/router';
 import AOS from 'aos';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    FooterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'Viktor Ens';
@@ -20,8 +28,8 @@ export class AppComponent implements OnInit {
     translate.use('de'); // Aktiviere die Standard-Sprache
   }
 
-  getLanguage(){
-    return this.translate.store.currentLang
+  getLanguage() {
+    return this.translate.store.currentLang;
   }
 
   ngOnInit(): void {
@@ -33,5 +41,4 @@ export class AppComponent implements OnInit {
     const img = new Image();
     img.src = 'https://time-to-quit.onrender.com';
   }
-
 }
