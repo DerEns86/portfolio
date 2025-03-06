52339f6e69efd101ff4dc6ce4be74624d98ac0f6
DerEns86 <v.ens86@yahoo.de>
2025-03-06 09:22:02 +0100

    Add JoinComponent with routing and basic template

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PlaygroundComponent } from './main-content/playground/playground.component';
+import { JoinComponent } from './main-content/join/join.component';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'playground', component: PlaygroundComponent },
+  { path: 'join', component: JoinComponent },
 ];
