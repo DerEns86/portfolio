10edef22c5b2bed14f6ad0c849278ac4161579b8
DerEns86 <v.ens86@yahoo.de>
2025-05-13 14:40:28 +0200

    Refactor code structure for improved readability and maintainability

--- portfolio/src/app/main-content/contact/contact.component.ts
+++ portfolio/src/app/main-content/contact/contact.component.ts
@@ -3,30 +3,35 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { ImprintComponent } from '../../imprint/imprint.component';
+import { ImprintComponent } from '../../pages/imprint/imprint.component';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, TranslateModule,RouterLink, ImprintComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    RouterLink,
+    ImprintComponent,
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
- 
-constructor(){}
+  constructor() {}
 
-mailSubmitted = false;
- contactData = {
+  mailSubmitted = false;
+  contactData = {
     name: '',
     email: '',
     message: '',
     checked: false,
-  }
-  
-http = inject(HttpClient)
+  };
 
+  http = inject(HttpClient);
 
   post = {
     endPoint: 'https://v-ens.de/sendMail.php',
@@ -40,11 +45,12 @@ http = inject(HttpClient)
   };
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid ) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+    if (ngForm.submitted && ngForm.form.valid) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
-            this.toggleMessage()
+            this.toggleMessage();
             ngForm.resetForm();
           },
           error: (error) => {
@@ -52,14 +58,13 @@ http = inject(HttpClient)
           },
           complete: () => console.info('send post complete'),
         });
-    } 
+    }
   }
 
- toggleMessage(){
-  this.mailSubmitted = true;
-  setTimeout(() => {
-    this.mailSubmitted = false;
-  }, 2000);
- }
-
+  toggleMessage() {
+    this.mailSubmitted = true;
+    setTimeout(() => {
+      this.mailSubmitted = false;
+    }, 2000);
+  }
 }
--- portfolio/src/app/main-content/skills/skills.component.ts
+++ portfolio/src/app/main-content/skills/skills.component.ts
@@ -1,31 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { SeperatorComponent } from '../../shared/components/seperator/seperator.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [ CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, SeperatorComponent],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-  skills = [
-    { name: 'Angular', iconPath: 'angular.png' },
-    { name: 'TypeScript', iconPath: 'ts.png' },
-    { name: 'JavaScript', iconPath: 'js.png' },
-    { name: 'Java', iconPath: 'java2.png' },
-    { name: 'Spring Boot', iconPath: 'spring-boot.svg' },
-    { name: 'HTML', iconPath: 'ts.png' },
-    { name: 'CSS', iconPath: 'css.png' },
-    { name: 'React', iconPath: 'react.png' },
-    { name: 'Git', iconPath: 'git.png' },
-    { name: 'MongoDB', iconPath: 'mongo.png' },
-    { name: 'Firebase', iconPath: 'firebase.png' },
-    { name: 'CI/CD with Github Actions', iconPath: 'github-2.png' },
-    { name: 'Scrum', iconPath: 'scrum.png' },
-    { name: 'Rest-API', iconPath: 'api.png' },
-    { name: 'Material Design', iconPath: 'material.png' },
-  ]
+  languages: string[] = ['Java', 'TypeScript', 'JavaScript'];
+  frameworks: string[] = ['Angular', 'Spring Boot', 'React', 'Vue.js'];
+  tools: string[] = [
+    'Git',
+    'Firebase',
+    'CI/CD ',
+    'Github Actions',
+    'Scrum',
+    'Material Design',
+  ];
+  databases: string[] = ['PostgeSQL', 'MySQL', 'MongoDB'];
+  other: string[] = ['HTML', 'CSS', 'Rest-API'];
+
+  // skills = [
+  //   { name: 'Angular', iconPath: 'angular.png' },
+  //   { name: 'TypeScript', iconPath: 'ts.png' },
+  //   { name: 'JavaScript', iconPath: 'js.png' },
+  //   { name: 'Java', iconPath: 'java2.png' },
+  //   { name: 'Spring Boot', iconPath: 'spring-boot.svg' },
+  //   { name: 'HTML', iconPath: 'ts.png' },
+  //   { name: 'CSS', iconPath: 'css.png' },
+  //   { name: 'React', iconPath: 'react.png' },
+  //   { name: 'Git', iconPath: 'git.png' },
+  //   { name: 'MongoDB', iconPath: 'mongo.png' },
+  //   { name: 'Firebase', iconPath: 'firebase.png' },
+  //   { name: 'CI/CD with Github Actions', iconPath: 'github-2.png' },
+  //   { name: 'Scrum', iconPath: 'scrum.png' },
+  //   { name: 'Rest-API', iconPath: 'api.png' },
+  //   { name: 'Material Design', iconPath: 'material.png' },
+  // ];
 }
