9269cb6aa81239f3b760bf4549f919bc4c07558d
DerEns86 <v.ens86@yahoo.de>
2025-05-14 12:13:36 +0200

    feat: refactor skills component to use dynamic skills list and improve structure

--- portfolio/src/app/models/skills.ts
+++ portfolio/src/app/models/skills.ts
@@ -1,4 +1,31 @@
-export class Skills {
-    public name: String = '';
-    public iconPath: String = '';
+export function skillsList() {
+  return [
+    { key: 'languages', values: ['Java', 'TypeScript', 'JavaScript'] },
+    {
+      key: 'frameworks',
+      values: [
+        'Angular',
+        'Spring Boot',
+        'TailwindCSS',
+        'Bootstrap',
+        'React',
+        'Vue.js',
+      ],
+    },
+    { key: 'databases', values: ['PostgeSQL', 'MySQL', 'MongoDB'] },
+    {
+      key: 'tools',
+      values: [
+        'Git',
+        'Firebase',
+        'CI/CD',
+        'Docker',
+        'Github Actions',
+        'Scrum',
+        'Material Design',
+      ],
+    },
+    { key: 'testing', values: ['JUnit', 'Jasmine', 'Integration Tests'] },
+    { key: 'other', values: ['HTML', 'CSS', 'RESTful APIs', 'RxJS'] },
+  ];
 }
